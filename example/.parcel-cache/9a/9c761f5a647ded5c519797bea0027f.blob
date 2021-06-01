52dd6e4ef6cc4d86c5db3e82124c0871195ec774
leonardomso <leonardomso11@gmail.com>
2021-05-31 21:47:00 -0300

    Example folder now running correctly. It does not have a 'build' command but this is someting we can add later. Now we'll be able to test the useRoover hooks in this folder and make sure everything is working correctly.

---
+++ example/.parcel-cache/9a/9c761f5a647ded5c519797bea0027f.blob
@@ -0,0 +1,1 @@
+module.exports.isNode = false;
--- example/index.tsx
+++ example/index.tsx
@@ -1,4 +1,3 @@
-import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {
@@ -13,18 +12,12 @@ import Source from "./components/Source";
 import Details from "./components/Details";
 import Controls from "./components/Controls";
 
-import { useRoover } from "../src";
-
-const src: string = "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3";
-
 const App = () => {
-  const { onToggle } = useRoover({ src });
-
   return (
     <ChakraProvider theme={theme}>
       <Box w="100%" h="100vh" display="flex" alignItems="center" justifyContent="center">
         <Grid templateRows="repeat(5, max-content)" templateColumns="repeat(2, 1fr)" gap={5} alignItems="center" justifyContent="center">
-          <Heading as="h1" letterSpacing="-0.03em">Audio using XState</Heading>
+          <Heading as="h1" letterSpacing="-0.03em">Roover</Heading>
           <Source gridRow="2 / 3" />
           <Controls />
           <Details />
